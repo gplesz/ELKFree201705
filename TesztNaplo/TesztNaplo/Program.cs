e6e1513af3a7734ca625e766f003765c288d56bd
gplesz <gabor.plesz@gmail.com>
2017-05-09 11:58:42 +0200

    #2: logstash config a timestamp helyes kitöltéséhez  + napló mennyiség felgyorsítása

--- TesztNaplo/TesztNaplo/Program.cs
+++ TesztNaplo/TesztNaplo/Program.cs
@@ -23,31 +23,34 @@ namespace TesztNaplo
             while (!Console.KeyAvailable)
             {
                 var val = r.Next(100);
+                Thread.Sleep(4);
 
                 if (val<60)
                 {
                     logger.Debug("Ez egy debug");
-                    Thread.Sleep(100);
                 }
                 else if (val < 80)
                 {
                     logger.Info("Ez egy info");
-                    Thread.Sleep(200);
                 }
                 else if (val < 90)
                 {
                     logger.Warn("Warning");
-                    Thread.Sleep(300);
                 }
                 else if (val < 97)
                 {
-                    logger.Error("Error");
-                    Thread.Sleep(400);
+                    try
+                    {
+                        throw new Exception("csibészkedem, dobok egy kivételt");
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error("Error", ex);
+                    }
                 }
                 else 
                 {
                     logger.Fatal("Fatal");
-                    Thread.Sleep(400);
                 }
             }
 
